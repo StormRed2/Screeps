6cf2f1b2c471fec07feae8affdbb7b048c8a7919
Florian Martin <florian.martin91@gmail.com>
2017-04-13 15:41:29 +0200

    Add upgrade spawning

--- main.js
+++ main.js
@@ -11,10 +11,22 @@ module.exports.loop = function () {
     }
 
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+    var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+    var builder = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+
+    console.clear();
+    console.log("Currently there are:");
     console.log('Harvesters: ' + harvesters.length);
+    console.log('Upgraders: ' + upgraders.length);
+    console.log('Builders: ' + builder.length);
 
     if(harvesters.length < 4) {
         var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
+        console.log('Spawning new harvester: ' + newName);
+    }
+
+    if(upgraders.length < 1) {
+        var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'upgrader'});
         console.log('Spawning new harvester: ' + newName);
     }
 
