d8593087f561fb11735d05875e4e502bd8223744
Florian Martin <florian.martin91@gmail.com>
2017-04-13 16:27:45 +0200

    Add new Logging

--- info.board.js
+++ info.board.js
@@ -2,7 +2,9 @@
  * Created by Florian on 4/13/2017.
  */
 
-
+var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
 
 
 var board = {
@@ -17,7 +19,9 @@ var board = {
         if (s == 0 || s == 1 || s == 2 || s == 3 || s == 4 || s == 5 || s == 6) {
             console.log();
             console.log();
-            console.log("TEST");
+            console.log("Harvesters" + harvesters.length);
+            console.log("Upgraders" + upgraders.length);
+            console.log("Builders" + builders.length);
 
 
         }
