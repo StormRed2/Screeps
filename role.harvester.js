34d5fedbb91cd247226b165cb4255aba916177cc
Florian Martin <florian.martin91@gmail.com>
2017-04-13 17:13:37 +0200

    Add more harvester work

--- role.harvester.js
+++ role.harvester.js
@@ -1,4 +1,6 @@
 var randomNum = Math.floor(Math.random() * (1 - 0 + 1)) + 0;
+var roleUpgrader = require('role.upgrader');
+var harvestWorkAvailable = false;
 
 var roleHarvester = {
 
@@ -6,8 +8,8 @@ var roleHarvester = {
     run: function(creep) {
         if(creep.carry.energy < creep.carryCapacity) {
             var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            if(creep.harvest(sources[randomNum]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[randomNum], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
         else {
@@ -15,14 +17,22 @@ var roleHarvester = {
                 filter: (structure) => {
                     return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
                         structure.energy < structure.energyCapacity;
+                    if(structure.energy < structure.energyCapacity){
+                        harvestWorkAvailable = true;
+                    }
                 }
             });
             if(targets.length > 0) {
-                if(creep.transfer(targets[randomNum], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[randomNum], {visualizePathStyle: {stroke: '#ffffff'}});
+                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
                 }
             }
         }
+            //if no work is left, the harvester should upgrade the controller
+        if(harvestWorkAvailable ==  false) {
+            creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
+        }
+
     }
 };
 
