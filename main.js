31ddf666131fb371e1fa0f0eed724b726c1ede4e
Florian Martin <florian.martin91@gmail.com>
2017-04-13 15:35:07 +0200

    Fix harvester count

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ module.exports.loop = function () {
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
     console.log('Harvesters: ' + harvesters.length);
 
-    if(harvesters.length < 2) {
+    if(harvesters.length < 4) {
         var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
         console.log('Spawning new harvester: ' + newName);
     }
