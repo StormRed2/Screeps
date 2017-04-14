75df4c42ea3c0943adbceaa6dd633d574a5d7d5c
Florian Martin <florian.martin91@gmail.com>
2017-04-14 16:32:11 +0200

    Add creep manager

---
+++ manager.creeps.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Florian on 4/14/2017.
+ * manages the creeps and their tasks
+ */
+
+
+var manager ={
+
+run: function ( ) {
+
+
+    var harvesterCount = factoryWorkload.getDynamicHarvestersCount();
+    var minersCount = factoryWorkload.getDynamicMinersCount();
+    var upgraderCount = factoryWorkload.getDynamicUpgradersCount();
+    var buildersCount = factoryWorkload.getDynamicBuildersCount();
+    var repairerCount = factoryWorkload.getDynamicRepairersCount();
+
+console.log("Test: am I'm running?");
+
+}
+
+
+
+}
+
+module.exports = managerCreeps;
